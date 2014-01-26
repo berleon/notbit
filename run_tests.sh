d68953ba5b5d0ca8514ad58823b3529224459934
jk <ulukaiz@hotmail.com>
2014-01-26 01:37:41 +0100

    Add script for running testcases. Modified README

---
+++ run_tests.sh
@@ -0,0 +1,20 @@
+#! /usr/bin/env bash
+
+test_file="*"
+if [ -n "$1" ]
+then
+  test_file=$1
+fi
+echo "$test_file"
+make check
+echo 
+echo "> Start running Tests..."
+find tests/ -maxdepth 1 -name "$test_file" -executable -type f | while read line;
+do
+  echo
+  echo "> Test >  $line"
+  echo 
+  $(echo $(pwd)/$line)
+done
+echo
+echo "> Finished Tests"
